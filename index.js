af5be033b4b5ceeacac6590a259376ee7751fd17
Phattaraphon30055 <6406021630055@fitm.kmutnb.ac.th>
2024-01-03 14:22:55 +0700

    update

--- index.js
+++ index.js
@@ -13,14 +13,15 @@ app.listen(port,
         console.log(`Example app listening at http://localhost:${port}`);
 
     });**/
-const express = require('express');
-const app = express();
-const port = 3000; // หรือพอร์ตที่คุณต้องการ
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    const express = require('express');
+    const app = express();
+    const port = 4000; // เปลี่ยนเป็นพอร์ตที่ไม่ถูกใช้งาน
+    
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+    
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+    
